e9337cb7d470c0c0d2fd2d4139da806e90946ad8
whaletradesweb <whaletradesweb@gmail.com>
2025-07-31 22:01:53 +0200

    Delete api/open-interest-debug.js

--- api/open-interest-debug.js
+++
@@ -1,6 +0,0 @@
-module.exports = async (req, res) => {
-  res.json({
-    message: "Open Interest Debug endpoint active",
-    note: "KV storage is currently disabled, only API fetch is used."
-  });
-};
