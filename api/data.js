78a5b22ebfbc217b434318a5c61ce65170e737a8
whaletradesweb <whaletradesweb@gmail.com>
2025-09-03 05:41:57 +0200

    Update data.js

--- api/data.js
+++ api/data.js
@@ -6,7 +6,7 @@ const { cacheGetSet, allow, axiosWithBackoff } = require("./lib/cacheAndLimit");
 
 module.exports = async (req, res) => {
   // Enhanced CORS headers for better compatibility
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://www.whaletrades.io');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept, Origin');
   res.setHeader('Cache-Control', 'public, s-maxage=60, stale-while-revalidate=300');
